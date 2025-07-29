82bf41d330f2f6131c01eb4c0da7f076f819e7e6
Chetan Gadhiya <chetangadhiya4939@gmail.com>
2025-07-29 09:48:59 +0530

    caeser cipher chnaged

--- System Software & Compiler Design/Lab/Lab1.c
+++ System Software & Compiler Design/Lab/Lab1.c
@@ -2,44 +2,93 @@
 // it lists the keyword used in written code in "input.txt";
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+// List of C keywords
+const char *keywords[] = {
+    "auto", "break", "case", "char", "const", "continue", "default", "do", "double",
+    "else", "enum", "extern", "float", "for", "goto", "if", "int", "long",
+    "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
+    "typedef", "union", "unsigned", "void", "volatile", "while", "_Bool", "_Complex", "_Imaginary"
+};
+
+int isKeyword(const char *word) {
+    for (int i = 0; i < sizeof(keywords)/sizeof(keywords[0]); i++) {
+        if (strcmp(word, keywords[i]) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+// DFA to validate identifier
+int isValidIdentifier(const char *str) {
+    int i = 0;
+    if (!(isalpha(str[0]) || str[0] == '_'))
+        return 0;
+    for (i = 1; str[i] != '\0'; i++) {
+        if (!(isalnum(str[i]) || str[i] == '_'))
+            return 0;
+    }
+    return 1;
+}
+
+int isDelimiter(char ch) {
+    return isspace(ch) || ch == ';' || ch == ',' || ch == '(' || ch == ')' ||
+           ch == '{' || ch == '}' || ch == '[' || ch == ']' || ch == '#' || 
+           ch == '<' || ch == '>' || ch == '+' || ch == '-' || ch == '*' || 
+           ch == '/' || ch == '=' || ch == '&' || ch == '|' || ch == '!' || 
+           ch == '%' || ch == '^' || ch == '~' || ch == '"' || ch == '\'';
+}
 
 int main() {
-    FILE *file;
-    char word[100];
+    FILE *fp;
+    char ch, buffer[MAX_LEN];
+    int i = 0;
 
-    const char *keywords[] = {
-        "auto", "break", "case", "char", "const", "continue", "default", "do",
-        "double", "else", "enum", "extern", "float", "for", "goto", "if",
-        "int", "long", "register", "return", "short", "signed", "sizeof", "static",
-        "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-    };
-
-    int num_keyword = sizeof(keywords)/sizeof(keywords[0]);
-
-    file = fopen("example.txt", "r");
-
-    if (file == NULL) {
-        printf("File not found or cannot be opened.\n");
+    fp = fopen("input.c", "r");
+    if (fp == NULL) {
+        printf("Error: Cannot open input.c file!\n");
         return 1;
     }
 
-    printf("Keyword Found : ");
+    printf("Keywords:\n");
+    printf("Identifiers:\n");
+    printf("Others:\n");
 
-    
-    while (fscanf(file, "%s", word) != EOF) {
-        for (int i = 0; i < num_keyword; i++){
-            if (strcmp(word, keywords[i]) == 0){
-                printf("%s\n", word);
+    while ((ch = fgetc(fp)) != EOF) {
+        if (isDelimiter(ch)) {
+            if (i > 0) {
+                buffer[i] = '\0';
+
+                if (isKeyword(buffer)) {
+                    printf("[Keyword]    : %s\n", buffer);
+                } else if (isValidIdentifier(buffer)) {
+                    printf("[Identifier] : %s\n", buffer);
+                } else {
+                    printf("[Others]     : %s\n", buffer);
+                }
+
+                i = 0;
             }
+
+            if (!isspace(ch)) {
+                // If not a space, consider as separate token
+                char op[2] = {ch, '\0'};
+                printf("[Others]     : %s\n", op);
+            }
+        } else {
+            buffer[i++] = ch;
         }
-        // putchar(ch);
     }
-    
 
-    fclose(file);
-
+    fclose(fp);
     return 0;
 }
+
 
 
 
